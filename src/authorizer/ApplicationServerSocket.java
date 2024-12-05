55ef19d8e8de9f4d7efb427ec1dab0c427437ac9
WIN-V3PJFOAH078\ludio <Ludionei Reis>
2024-12-05 12:56:16 -0300

    no message

--- src/authorizer/ApplicationServerSocket.java
+++ src/authorizer/ApplicationServerSocket.java
@@ -1,21 +1,15 @@
 package authorizer;
 
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import authorizer.infrastructure.protocol.ISO8583Request0200;
-import authorizer.infrastructure.protocol.ISO8583Response0210;
-import authorizer.service.PaymentAuthorizerService;
-import authorizer.usecase.PaymentAuthorizer;
 
 public class ApplicationServerSocket {
 
     private static final int PORT = 12345;
 
-    public static void main(String[] args) {
+    @SuppressWarnings("resource")
+	public static void main(String[] args) {
         
         try {
         	
